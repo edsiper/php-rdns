1895babe2947da48f2cd217a614c7a70f0ea6125
Alexander Solovets <asolovets@gmail.com>
2014-07-25 00:56:53 +1100

    Work in progress.

--- rdns.c
+++ rdns.c
@@ -2,7 +2,10 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
 #include <php.h>
+#include "rdns.h"
+#include "rdns_curve.h"
 #include "rdns_ev.h"
 
 #define PHP_RDNS_VERSION "0.1.0"
@@ -17,9 +20,16 @@
 
 typedef struct {
   zend_object obj;
+  zval *result;
   struct ev_loop *loop;
   struct rdns_resolver *resolver;
+  size_t nreq;
 } php_rdns_t;
+
+typedef struct {
+  php_rdns_t *rdns;
+  size_t index;
+} php_rdns_context_t;
 
 static zend_class_entry *rdns_ce = NULL;
 
@@ -154,8 +164,11 @@ static PHP_METHOD(RDNS, __construct)
     php_error_docref(NULL TSRMLS_CC, E_ERROR, "could not create resolver structure");
   }
 
+  i_obj->nreq = 0;
   i_obj->loop = ev_default_loop(0);
   rdns_bind_libev(i_obj->resolver, i_obj->loop);
+  rdns_resolver_set_log_level (i_obj->resolver, RDNS_LOG_DEBUG);
+  rdns_resolver_set_max_io_uses (i_obj->resolver, 1, 0.1);
 }
 /* }}} */
 
@@ -180,27 +193,99 @@ static PHP_METHOD(RDNS, addServer)
 }
 /* }}} */
 
+static void
+rdns_reply_callback(struct rdns_reply *reply, void *arg)
+{
+  struct rdns_reply_entry *entry;
+  char ipstr[INET6_ADDRSTRLEN + 1];
+  zval *result, *result_item;
+  php_rdns_context_t *context = (php_rdns_context_t *) arg;
+  struct rdns_request_name *name = rdns_request_get_name(reply->request, NULL);
 
-/* {{{ RDNS::addRequest(string $hostname, int $type, float $timeout) */
+  MAKE_STD_ZVAL(result);
+  array_init(result);
+  if (reply->code == RDNS_RC_NOERROR) {
+    entry = reply->entries;
+    while (entry != NULL) {
+      MAKE_STD_ZVAL(result_item);
+      array_init(result_item);
+      add_assoc_string_ex(result_item, ZEND_STRS("host"),
+                          name->name, 1);
+      add_assoc_string_ex(result_item, ZEND_STRS("class"), "IN", 1);
+      add_assoc_long(result_item, "ttl", entry->ttl);
+
+      if (entry->type == RDNS_REQUEST_A) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "A", 1);
+        inet_ntop (AF_INET, &entry->content.a.addr, ipstr, sizeof (ipstr));
+        add_assoc_string_ex(result_item, ZEND_STRS("ip"), ipstr, 1);
+      } else if (entry->type == RDNS_REQUEST_AAAA) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "AAAA", 1);
+        inet_ntop (AF_INET6, &entry->content.aaa.addr, ipstr, sizeof ipstr);
+        add_assoc_string_ex(result_item, ZEND_STRS("ipv6"), ipstr, 1);
+      } else if (entry->type == RDNS_REQUEST_MX) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "MX", 1);
+        add_assoc_long(result_item, "pri", entry->content.mx.priority);
+        add_assoc_string_ex(result_item, ZEND_STRS("target"),
+                            entry->content.mx.name, 1);
+      } else if (entry->type == RDNS_REQUEST_NS) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "NS", 1);
+        add_assoc_long(result_item, "pri", entry->content.mx.priority);
+        add_assoc_string_ex(result_item, ZEND_STRS("target"),
+                            entry->content.mx.name, 1);
+      } else if (entry->type == RDNS_REQUEST_TXT) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "TXT", 1);
+        add_assoc_string_ex(result_item, ZEND_STRS("txt"),
+                            entry->content.txt.data, 1);
+      } else if (entry->type == RDNS_REQUEST_SRV) {
+        add_assoc_string_ex(result_item, ZEND_STRS("type"), "SRV", 1);
+        add_assoc_string_ex(result_item, ZEND_STRS("target"),
+                            entry->content.srv.target, 1);
+        add_assoc_long(result_item, "pri", entry->content.srv.priority);
+        add_assoc_long(result_item, "weight", entry->content.srv.weight);
+        add_assoc_long(result_item, "port", entry->content.srv.port);
+      }
+      entry = entry->next;
+      add_next_index_zval(result, result_item);
+    }
+  }
+  add_index_zval(context->rdns->result, context->index, result);
+
+  if (!--context->rdns->nreq) {
+    rdns_resolver_release(context->rdns->resolver);
+  }
+}
+
+/* {{{ bool RDNS::addRequest(string $hostname, int $type, float $timeout) */
 static PHP_METHOD(RDNS, addRequest)
 {
   char *hostname;
   size_t hostname_len;
-  long type, timeout;
+  long type;
+  double timeout;
+  php_rdns_context_t *context;
 
   RDNS_INIT_VARS;
 
-  if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sll",
-                            &hostname, &hostname_len, &type, &timeout) == FAILURE) {
-    ZVALL_NULL(object);
+  if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+                            "sld",
+                            &hostname,
+                            &hostname_len,
+                            &type,
+                            &timeout) == FAILURE) {
     return;
   }
 
-  php_printf("%s %ld %ld\n", hostname, type, timeout);
-
   RDNS_FETCH_OBJECT;
 
-  RETURN_FALSE;
+  context = (php_rdns_context_t *) malloc(sizeof *context);
+  context->rdns = i_obj;
+  context->index = i_obj->nreq++;
+  if (!context->index) {
+    rdns_resolver_init(i_obj->resolver);
+  }
+  RETURN_BOOL(rdns_make_request_full(i_obj->resolver, rdns_reply_callback,
+                                     context, timeout, 1, 1,
+                                     hostname, type));
 }
 /* }}} */
 
@@ -217,5 +302,10 @@ static PHP_METHOD(RDNS, getReplies)
   RDNS_FETCH_OBJECT;
 
   array_init(return_value);
+  i_obj->result = return_value;
+  if (i_obj->nreq) {
+    ev_run(i_obj->loop, 0);
+    /* ev_loop_destroy(i_obj->loop); */
+  }
 }
 /* }}} */
